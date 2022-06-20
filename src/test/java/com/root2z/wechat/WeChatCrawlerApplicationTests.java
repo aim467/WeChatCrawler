cfa15ddd36e44e856ae5abd0c6b7c82a120d6fa3
aim467 <709544711@qq.com>
2022-06-20 15:00:03 +0800

    feat: 初始化仓库

---
+++ src/test/java/com/root2z/wechat/WeChatCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root2z.wechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeChatCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
